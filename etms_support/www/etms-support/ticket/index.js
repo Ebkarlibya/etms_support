338eb9784365c42ec5d483999869c6be1183b26b
Imad Abdou <igentle.appletec@gmail.com>
2021-12-26 14:27:57 +0100

    add ticket replays and basic replays listing

--- etms_support/www/etms-support/ticket/index.js
+++ etms_support/www/etms-support/ticket/index.js
@@ -1,1 +1,17 @@
-console.log("index.js")
+var replayArea = document.querySelector("#etms-replay-textarea");
+var submitReplay = document.querySelector("#etms-submit-replay");
+var closeTicket = document.querySelector("#etms-close-ticket");
+var alertSuccess = document.querySelector(".alert-success");
+var alertError = document.querySelector(".alert-danger");
+
+
+
+// Submit replay
+submitReplay.addEventListener("click", function() {
+    alertSuccess.hidden = !alertSuccess.hidden
+});
+
+// Close ticket
+submitReplay.addEventListener("click", function() {
+
+});
--- etms_support/www/etms-support/ticket/index.py
+++ etms_support/www/etms-support/ticket/index.py
@@ -6,20 +6,7 @@ def get_context(ctx):
     query = frappe.request.query_string
     target_ticket = query.decode('utf-8').split("=")[1]
     
-    ticket = frappe.get_all(
-        "Tickets", [
-            "name",
-            "creation",
-            "department",
-            "subject",
-            "description",
-            "user",
-            "status"
-        ],
-        filters={
-            "name": target_ticket
-        }
-    )[0]
+    ticket = frappe.get_doc("Tickets", target_ticket)
 
     ctx['target_ticket'] = target_ticket
     ctx['ticket'] = ticket
