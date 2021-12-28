d003469cf267f0e5f39adf325e59727701b01b1c
Imad Abdou <igentle.appletec@gmail.com>
2021-12-28 11:20:34 +0100

    add attachment functionality to new-ticket page

--- etms_support/www/etms-support/new-ticket/index.js
+++ etms_support/www/etms-support/new-ticket/index.js
@@ -1,6 +1,7 @@
 var department = document.querySelector("#etms-department");
 var subject = document.querySelector("#etms-subject");
 var description = document.querySelector("#etms-description");
+var attachFile = document.querySelector("#etms-attach-file");
 var submitTicket = document.querySelector("#etms-submit-ticket");
 var alertSuccess = document.querySelector(".alert-success");
 var alertError = document.querySelector(".alert-danger");
@@ -12,16 +13,20 @@ submitTicket.addEventListener("click", function() {
     var dep = department.value;
     var subj = subject.value;
     var desc = description.value;
-
+    var file = attachFile.files[0];
     if (dep && !isNaN(parseInt(dep)) && subject && desc) {
         submitTicket.disabled = true;
         frappe.call({
             method: 'etms_support.tickets.submit_ticket',
             args: { departmentIndex: dep, subject: subj, description: desc },
-            callback: function(r) {
+            callback: async function(r) {
                 console.log(r.message);
                 if (r.message.name) {
                     let ticket_name = r.message.name;
+                    if(file) {
+                        var r = await upload_file(file, "Tickets", ticket_name);
+                    }
+                    console.log(r);
                     alertSuccess.hidden = false;
                     alertSuccess.innerHTML = "Your Ticket: " + ticket_name + " Submitted, Thank you!";
                     setTimeout(function() {
@@ -37,3 +42,43 @@ submitTicket.addEventListener("click", function() {
 
     }
 });
+
+
+function upload_file(file, doctype, docname) {
+    return new Promise(function(resolve, reject) {
+
+        var xhr = new XMLHttpRequest();
+        var fd = new FormData();
+        var base_url = window.location.origin
+        var file_name = "todo-rec-" + file.name;
+        var file_url = base_url + "/private/files/" + file_name;
+    
+    
+        xhr.open("POST", "/api/method/upload_file", true);
+    
+        xhr.setRequestHeader("X-Frappe-CSRF-Token", frappe.csrf_token);
+        xhr.setRequestHeader("ContentType", "Application/json");
+    
+        fd.append("file", file, file.name);
+        fd.append("is_private", 1);
+        fd.append("file_name", file_name);
+        fd.append("file_url", file_url);
+        if(doctype && docname) {
+            fd.append("doctype", doctype);
+            fd.append("docname", docname);
+        }
+        xhr.send(fd);
+    
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState == 4 && xhr.status == 200) {
+            try{
+                var res = JSON.parse(xhr.response).message;
+                    resolve(res);
+            } catch {
+                console.log('reject', res);
+                reject(res)
+            }
+          } 
+        };
+    })
+}
--- etms_support/www/etms-support/ticket/index.py
+++ etms_support/www/etms-support/ticket/index.py
@@ -1,5 +1,5 @@
 import frappe
-from frappe import data_migration
+from urllib.parse import urljoin
 
 def get_context(ctx):
     user = frappe.session.user
@@ -7,6 +7,12 @@ def get_context(ctx):
     target_ticket = query.decode('utf-8').split("=")[1]
     
     ticket = frappe.get_doc("Tickets", target_ticket)
+    attachment_path = frappe.get_all("File", fields="file_url", filters={
+        "attached_to_doctype": "Tickets",
+        "attached_to_name": ticket.name
+    })
+    if len(attachment_path) > 0:
+        ctx['attachment_url'] = urljoin(ctx.url, attachment_path[0].file_url)
 
     ctx['target_ticket'] = target_ticket
     ctx['ticket'] = ticket
