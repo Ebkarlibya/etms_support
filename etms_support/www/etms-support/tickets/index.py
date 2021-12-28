6be64ea921a7ebc1d748d5e3f71170a53dfe293d
Imad Abdou <igentle.appletec@gmail.com>
2021-12-28 18:06:13 +0100

    list tickets for non guest users only

--- etms_support/www/etms-support/tickets/index.py
+++ etms_support/www/etms-support/tickets/index.py
@@ -3,7 +3,8 @@ from frappe import data_migration
 
 def get_context(ctx):
     ctx.no_cache = 1
-    user = frappe.session.user
+    user = frappe.get_doc("User", frappe.session.user)
+
     tickets = frappe.get_all(
         "Tickets",
         fields=[
@@ -13,6 +14,8 @@ def get_context(ctx):
             "subject",
             "description",
             "status"
-        ])
+        ], filters={
+            "user_name": frappe.session.user
+        })
     ctx['company'] = frappe.defaults.get_user_default("Company")
     ctx['tickets'] = tickets
