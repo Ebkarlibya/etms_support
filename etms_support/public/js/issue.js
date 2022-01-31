09798d9b150cf4b908a18081c95fa4f868b05034
Imad Abdou <igentle.appletec@gmail.com>
2022-01-31 14:42:15 +0100

    disable Issue attaching files / disable comments/reply

--- etms_support/public/js/issue.js
+++ etms_support/public/js/issue.js
@@ -2,6 +2,11 @@ frappe.ui.form.on("Issue", {
 	refresh: function(frm) {
 		frm.add_custom_button(frappe._("View in Web Chat"), function() {
 			window.open("/etms-support/ticket?name="+frm.doc.name, "_blank").focus();
-		})
+		});
+
+		document.querySelector('.form-attachments').remove();
+		document.querySelector('.comment-box').remove();
+		document.querySelector('.form-footer').remove();
+		
 	}
 })
